e82350e36157dc1af05d2490750123f8daea2c5c
Vishvajeet590 <42716731+Vishvajeet590@users.noreply.github.com>
2021-06-06 21:18:28 +0530

    V1.1

--- app/src/main/java/com/vishvajeet590/python/console/Launch.java
+++ app/src/main/java/com/vishvajeet590/python/console/Launch.java
@@ -15,6 +15,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.chaquo.python.console.R;
@@ -23,6 +28,9 @@ public class Launch extends AppCompatActivity {
     Button grant,skip;
     private static final int SMS_PERMISSION_CODE = 100;
      boolean res;
+     TextView text1,text2,text3;
+     ImageView image;
+     ProgressBar bar ;
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -32,9 +40,25 @@ public class Launch extends AppCompatActivity {
         ConstraintLayout layout = findViewById(R.id.launchLa);
         grant = findViewById(R.id.grant);
         skip = findViewById(R.id.skip);
+        text1 = findViewById(R.id.textView2);
+        text2 = findViewById(R.id.textView3);
+        text3 = findViewById(R.id.textView4);
+        image = findViewById(R.id.imageView);
+        bar = findViewById(R.id.progressBar);
+
         boolean result = false;
         result = checkPermission(Manifest.permission.RECEIVE_SMS,SMS_PERMISSION_CODE);
-        if (result == true) layout.setVisibility(View.VISIBLE);
+
+        if (result == false){
+            grant.setVisibility(View.VISIBLE);
+            skip.setVisibility(View.VISIBLE);
+            text1.setVisibility(View.VISIBLE);
+            text2.setVisibility(View.VISIBLE);
+            text3.setVisibility(View.VISIBLE);
+            image.setVisibility(View.VISIBLE);
+            bar.setVisibility(View.INVISIBLE);
+        }
+
         grant.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
