2185a9d4c6fe356da3f7a7bfd58c8652a05f7135
Vishvajeet590 <42716731+Vishvajeet590@users.noreply.github.com>
2021-06-06 21:01:50 +0530

    V1.1

---
+++ app/src/main/java/com/vishvajeet590/python/console/Launch.java
@@ -0,0 +1,95 @@
+package com.vishvajeet590.python.console;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.chaquo.python.console.R;
+
+public class Launch extends AppCompatActivity {
+    Button grant,skip;
+    private static final int SMS_PERMISSION_CODE = 100;
+     boolean res;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+        ConstraintLayout layout = findViewById(R.id.launchLa);
+        grant = findViewById(R.id.grant);
+        skip = findViewById(R.id.skip);
+        boolean result = false;
+        result = checkPermission(Manifest.permission.RECEIVE_SMS,SMS_PERMISSION_CODE);
+        if (result == true) layout.setVisibility(View.VISIBLE);
+        grant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean r = checkPermission(Manifest.permission.RECEIVE_SMS,SMS_PERMISSION_CODE);
+            }
+        });
+
+        skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+    }
+
+    public boolean checkPermission(String permission, int requestCode)
+    {
+        res = false;
+        boolean result = false;
+        // Checking if permission is not granted
+        if (ContextCompat.checkSelfPermission(Launch.this, permission) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(Launch.this, new String[] { permission }, requestCode);
+        }
+        else {
+            res = true;
+            result = true;
+            openNewActivity();
+            //Toast.makeText(Launch.this, "Permission already granted", Toast.LENGTH_SHORT).show();
+        }
+        return result;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == SMS_PERMISSION_CODE) {
+
+            // Checking whether user granted the permission or not.
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                // Showing the toast message
+                Toast.makeText(Launch.this, "SMS Permission Granted", Toast.LENGTH_SHORT).show();
+                res = true;
+                openNewActivity();
+            } else {
+                res = false;
+                Toast.makeText(Launch.this, "SMS Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    public void openNewActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
